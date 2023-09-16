8ae59312121647d137345f276aa8a4c398c94a4d
Lucas Vitor <lucasvitorfc@gmail.com>
2023-09-16 12:59:40 -0300

    userinterface(materialize)

--- admin/createuser.php
+++ admin/createuser.php
@@ -1,46 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="../materialize/css/materialize.min.css"  media="screen,projection"/>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Criar Usuário</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <!--
     <nav>
-        <ul>
+        <ul class="sidenavbar">
             <li><a href="./createuser.php">Criar Usuário</a></li>
             <li><a href="./user.php">Relatório de Usuários</a></li>
         </ul>
     </nav>
+    -->
     <div class="container">
         <h1>Criar Usuário</h1>
         <form method="post">
             <div>
                 <label for="nome">Nome do Usuário</label>
-                <input id="nome" name="nome" type="text">
+                <input id="nome" name="nome" type="text" autocomplete="off" required>
             </div>
             <div>
                 <label for="cpf">CPF</label>
-                <input id="cpf" name="cpf" type="text">
+                <input id="cpf" name="cpf" type="text" autocomplete="off" required>
             </div>
             <div>
                 <label for="telefone">Telefone</label>
-                <input id="telefone" name="telefone" type="text">
+                <input id="telefone" name="telefone" type="text" autocomplete="off" required>
             </div>
             <div>
                 <label for="endereco">Endereço</label>
-                <input id="endereco" name="endereco" type="text">
+                <input id="endereco" name="endereco" type="text" autocomplete="off" required>
             </div>
-            <div>
+            <div class="row">
             <label for="nivelAcesso">Nível de Acesso</label>
             <select id="nivelAcesso" name="nivelAcesso">
                 <option value="0">Usuário</option>
                 <option value="1">Administrador</option>
             </select>
             </div>
-            <div>
-                <input name="submit" type="submit" value="Criar Usuário">
+            <div class="row">   
+                <button onclick="location.href='./user.php';" class="btn light-blue accent-4">Relatório</button>
+                <button class="btn light-blue accent-4" type="submit" name="submit">Criar Usuário</button>
             </div>
         </form>
         <?php
@@ -64,5 +69,6 @@
 
         ?>
     </div>
+    <script type="text/javascript" src="../materialize/js/materialize.min.js"></script>
 </body>
 </html>
--- admin/user.php
+++ admin/user.php
@@ -2,88 +2,114 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="../materialize/css/materialize.min.css"  media="screen,projection"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>Relatório</title>
 </head>
 <body>
-    <nav>
-        <ul>
-            <li><a href="./createuser.php">Criar Usuário</a></li>
-            <li><a href="./user.php">Relatório de Usuários</a></li>
-        </ul>
-    </nav>
+    <!--
+    <div class="row">
+        <div>
+            <nav>
+                <ul>
+                    <li><a href="./createuser.php">Criar Usuário</a></li>
+                    <li><a href="./user.php">Relatório de Usuários</a></li>
+                </ul>
+            </nav>
+        </div>  
+    </div>
+    -->
+    
+    
     <div class="container">
         <h1>Relatório de Usuários</h1>
         <form method="post">
             <label for="search">Pesquisar por:</label>
-            <input type="text" id="search" name="pesquisa">
+            <input type="text" id="search" name="pesquisa" autocomplete="off">
             <label for="consultaPor">Consultar por: </label>
-            <select id="consultaPor" name="consultaPor">
-                <option value="id">ID</option>
-                <option selected value="nome">Nome</option>
-                <option value="cpf">CPF</option>
-                <option value="telefone">Telefone</option>
-            </select>
-            <input type="submit" name="submit" value="Pesquisar">
+
+            <div class="input-field col s12">
+                <select id="consultaPor" name="consultaPor">
+                    <option value="" selected disable>Filtro da consulta</option>
+                    <option value="id">ID</option>
+                    <option value="nome">Nome</option>
+                    <option value="cpf">CPF</option>
+                    <option value="telefone">Telefone</option>
+                </select>
+            </div>
+            
+            <div class="row">
+                <button onclick="location.href='./createuser.php';" class="btn light-blue accent-4">Relatório</button>
+                <button class="btn light-blue accent-4" type="submit" name="submit">Pesquisar</button>
+            </div>
         </form>
     
-        <table>
-            <thead>
-                <tr>
-                    <td>ID</td>
-                    <td>Nome</td>
-                    <td>CPF</td>
-                    <td>Telefone</td>
-                    <td>Endereço</td>
-                    <td>Nível de Acesso</td>
-                    <td>Editar</td>
-                    <td>Excluir</td>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-    
-                    include 'connect.php';
-                    include 'classes.php';
-                    
-                    if(!isset($_POST['submit']))
-                    {
-                        // na duvida, ve a documentaçao pra classe e metodos estáticos no php
-                        $rows = User::ReadUser($mysqli);
-                        EasyTable::DisplayTable($rows);
-                    }
-                    else
-                    {
-                        $consulta = $_POST["consultaPor"];
-                        $equals = $_POST["pesquisa"];
-    
-                        switch ($consulta)
-                        {
-                            case "id":
-                                $search = "id_usuario";
-                                break;
-                            case "nome":
-                                $search = "nome_usuario";
-                                break;
-                            case "cpf":
-                                $search = "cpf";
-                                break;
-                            case "telefone":
-                                $search = "telefone";
-                                break;
-                            default:
-                                $search = "nome_usuario";
-                        }
+        <div class="row">
+            <table class="stripped centered">
+                <thead>
+                    <tr>
+                        <td>ID</td>
+                        <td>Nome</td>
+                        <td>CPF</td>
+                        <td>Telefone</td>
+                        <td>Endereço</td>
+                        <td>Nível de Acesso</td>
+                        <td>Editar</td>
+                        <td>Excluir</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+        
+                        include 'connect.php';
+                        include 'classes.php';
                         
-    
-                        $rows = User::ReadUser($mysqli, $search, $equals);
-                        EasyTable::DisplayTable($rows);
-                    }
-    
-                ?>  
-            </tbody>
-        </table>
+                        if(!isset($_POST['submit']))
+                        {
+                            // na duvida, ve a documentaçao pra classe e metodos estáticos no php
+                            $rows = User::ReadUser($mysqli);
+                            EasyTable::DisplayTable($rows);
+                        }
+                        else
+                        {
+                            $consulta = $_POST["consultaPor"];
+                            $equals = $_POST["pesquisa"];
+        
+                            switch ($consulta)
+                            {
+                                case "id":
+                                    $search = "id_usuario";
+                                    break;
+                                case "nome":
+                                    $search = "nome_usuario";
+                                    break;
+                                case "cpf":
+                                    $search = "cpf";
+                                    break;
+                                case "telefone":
+                                    $search = "telefone";
+                                    break;
+                                default:
+                                    $search = "nome_usuario";
+                            }
+                            
+        
+                            $rows = User::ReadUser($mysqli, $search, $equals);
+                            EasyTable::DisplayTable($rows);
+                        }
+        
+                    ?>  
+                </tbody>
+            </table>
+        </div>
     </div>
+    <script type="text/javascript" src="../materialize/js/materialize.min.js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var sel = document.querySelectorAll('select');
+            M.FormSelect.init(sel)});
+    </script>
 </body>
 </html>
